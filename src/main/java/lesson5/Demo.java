5bbf8a751d129953e3cbf81aa39ef589879a7ae5
oleg.maliy14@gmail.com <q2301299266>
2019-07-29 23:04:31 +0300

    jdbc -2- 29/07/2019

---
+++ src/main/java/lesson5/Demo.java
@@ -0,0 +1,29 @@
+package lesson5;
+
+import org.hibernate.Session;
+
+public class Demo {
+    public static void main(String[] args) {
+        save();
+    }
+
+    public static void save(){
+        Session session = new HibernateUtils().createSessionFactory().openSession();
+
+        session.getTransaction().begin();
+
+        Product product = new Product();
+        product.setId(100);
+        product.setName("table");
+        product.setDescription("grey and blue");
+        product.setPrice(70);
+
+        session.save(product);
+
+        session.getTransaction().commit();
+
+        System.out.println("Done");
+
+        session.close();
+    }
+}
